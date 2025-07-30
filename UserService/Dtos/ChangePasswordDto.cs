35b2dbeb45a943ef708e9bd24ef1d116e1a8e3a5
Dekel Rafian <77198015+dekel5030@users.noreply.github.com>
2025-07-30 15:22:01 +0300

    Delete UserService directory

--- UserService/Dtos/ChangePasswordDto.cs
+++
@@ -1,9 +0,0 @@
-namespace UserService.Dtos
-{
-    public class ChangePasswordDto
-    {
-        public string CurrentPassword { get; set; } = string.Empty;
-        public string NewPassword { get; set; } = string.Empty;
-        public string ConfirmNewPassword { get; set; } = string.Empty;
-    }
-}
