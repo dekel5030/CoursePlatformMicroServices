47c710e59c4c8cf434a1aaf59e2c24d1845167ad
Dekel5030 <dekel5030@gmail.com>
2025-08-20 18:07:06 +0300

    (service orderservice) first file structure using clean architecture

---
+++ OrderService/Domain/Orders/Money.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Orders;
+
+public class Money
+{
+    public decimal Amount { get; private set; }
+    public string Currency { get; private set; }
+
+    public Money(decimal amount, string currency)
+    {
+        if (string.IsNullOrWhiteSpace(currency))
+            throw new ArgumentException("Currency cannot be null or empty.", nameof(currency));
+
+        Amount = amount;
+        Currency = currency;
+    }
+
+    public override string ToString() => $"{Amount} {Currency}";
+}
---
+++ OrderService/SharedKernel/Orders/OrderItemId.cs
@@ -0,0 +1,3 @@
+﻿namespace SharedKernel.Orders;
+
+public record struct OrderItemId(Guid value);
