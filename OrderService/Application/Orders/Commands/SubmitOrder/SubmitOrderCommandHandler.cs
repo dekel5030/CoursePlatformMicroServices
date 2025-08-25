7d1943bb78acd9fdca2bd3380aae771eb4321008
Dekel Rafian <77198015+dekel5030@users.noreply.github.com>
2025-08-25 12:04:18 +0300

    Feature/orderservice/create order (#26)
    * (feature orderservice) Added submit order handler in application layer and web api entry
    
    * (bugfix orderservice/submit-order-command-handler) Fixed bug where u could submit empty orders

---
+++ OrderService/Application/Orders/Commands/SubmitOrder/SubmitOrderCommandHandler.cs
@@ -0,0 +1,81 @@
+﻿using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Domain.Orders;
+using Domain.Orders.Errors;
+using SharedKernel;
+using SharedKernel.Customers;
+using SharedKernel.Products;
+
+namespace Application.Orders.Commands.SubmitOrder;
+
+public sealed class SubmitOrderCommandHandler : ICommandHandler<SubmitOrderCommand, Guid>
+{
+    private readonly IApplicationDbContext _dbContext;
+
+    public SubmitOrderCommandHandler(IApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Result<Guid>> Handle(SubmitOrderCommand command, CancellationToken cancellationToken)
+    {
+        SubmitOrderDto dto = command.Dto;
+        //if (await _dbContext.Customers.FindAsync(dto.CustomerId, cancellationToken) is null)
+        //{
+        //    return Result.Failure(OrderErrors.CustomerNotFound);
+        //}
+
+        Result<Order> result = Order.Create(new CustomerId(dto.CustomerId));
+
+        if (result.IsFailure)
+        {
+            return Result.Failure<Guid>(result.Error!);
+        }
+
+        Order order = result.Value;
+
+        foreach (SubmitOrderItemDto itemDto in dto.Products)
+        {
+            //Product product = await _dbContext.Products.AsNoTracking()
+            //    .SingleOrDefaultAsync(x => x.Id == productId, cancellationToken);
+
+            //if (product is null)
+            //{
+            //    return Result.Failure(ProductErrors.ProductNotFound);
+            //}
+
+            //Resukt<LineItem> lineItemResult = LineItem.Create(
+            //    product.Id,
+            //    quantity,
+            //    new Sku(product.Id),
+            //    product.Name,
+            //    product.Price);
+
+
+            Result<LineItem> lineItemResult = LineItem.Create(
+                new ProductId(itemDto.Id),
+                itemDto.Quantity,
+                "name",
+                Money.Zero());
+
+            if (lineItemResult.IsFailure)
+            {
+                return Result.Failure<Guid>(lineItemResult.Error!);
+            }
+
+            order.AddLine(lineItemResult.Value);
+        }
+
+        await _dbContext.Orders.AddAsync(order, cancellationToken);
+        await _dbContext.SaveChangesAsync();
+        Result<Order> submitResult = order.Submit();
+
+        if (submitResult.IsFailure)
+        {
+            return Result.Failure<Guid>(submitResult.Error!);
+        }
+
+        return Result.Success(order.Id.Value);
+    }
+}
+
