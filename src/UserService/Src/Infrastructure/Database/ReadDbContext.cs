7af9eaa407ca080466d16ee258506d846191d030
Dekel Rafian <77198015+dekel5030@users.noreply.github.com>
2025-10-07 18:44:27 +0300

    Refactor/userservice/add infra layer (#81)
    * (feature orderservice) uncommented section
    
    * (test orderservice) added order aggregate unit tests
    
    * (refactor orderservice) reorganized folder structure
    
    * (refactor userservice) Added first implementation of the infra layer

---
+++ src/UserService/Src/Infrastructure/Database/ReadDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Application.Abstractions.Data;
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database;
+
+public class ReadDbContext : DbContext, IReadDbContext
+{
+    public ReadDbContext(DbContextOptions<ReadDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DependencyInjection).Assembly);
+    }
+}
---
+++ src/UserService/Src/Infrastructure/Users/UserConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Users;
+using Domain.Users.Primitives;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Users;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    private const int _emailMaxLength = 256;
+    private const string _tableName = "users";
+
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable(_tableName);
+
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.Id)
+            .HasConversion(
+                id => id.Value,
+                value => new UserId(value));
+
+        builder.Property(u => u.Email).IsRequired().HasMaxLength(_emailMaxLength);
+        builder.HasIndex(u => u.Email).IsUnique();
+
+        builder.ComplexProperty(user => user.FullName);
+        builder.ComplexProperty(user => user.PhoneNumber);
+    }
+}
