fb223d51061adbbd6d550375e7a77aa6ee738066
Dekel5030 <dekel5030@gmail.com>
2025-10-07 16:37:36 +0300

    (refactor userservice) Added first implementation of application layer

---
+++ src/UserService/Src/Application/Abstractions/Data/IWriteDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Abstractions.Data;
+
+public interface IWriteDbContext
+{
+    DbSet<User> Users { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
---
+++ src/UserService/Src/Application/Users/Queries/GetUserByid/UserReadDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Users.Queries.GetUserByid;
+
+public record UserReadDto(
+    Guid Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    DateTime? DateOfBirth,
+    string? PhoneNumber);
