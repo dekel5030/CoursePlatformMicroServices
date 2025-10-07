781e270a34c8ad05207c49ddbbc068abbfbd484d
Dekel Rafian <77198015+dekel5030@users.noreply.github.com>
2025-10-07 19:37:28 +0300

    Refactor/userservice/add presentation layer (#82)
    * (feature orderservice) uncommented section
    
    * (test orderservice) added order aggregate unit tests
    
    * (refactor orderservice) reorganized folder structure
    
    * (refactor userservice) Added first implementation of the web api pressentation layer

---
+++ src/UserService/Src/Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Users.Queries.GetUserByid;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IQueryHandler<GetUserByIdQuery, UserReadDto>, GetUserByIdQueryHandler>();
+
+        return services;
+    }
+}
