a52ffb416e5dce8227e29403e4b7932eca121962
Dekel5030 <dekel5030@gmail.com>
2025-10-07 19:37:06 +0300

    (refactor userservice) Added first implementation of the web api pressentation layer

---
+++ src/UserService/Src/Web.Api/Endpoints/Users/GetUserById.cs
@@ -0,0 +1,25 @@
+﻿
+using Application.Abstractions.Messaging;
+using Application.Users.Queries.GetUserByid;
+using Kernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Users;
+
+public class GetUserById : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/api/users/{id:guid}", async (
+            Guid id, 
+            IQueryHandler<GetUserByIdQuery, UserReadDto> handler,
+            CancellationToken cancellationToken = default) =>
+        {
+            var query = new GetUserByIdQuery(id);
+            Result<UserReadDto> result = await handler.Handle(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+    }
+}
