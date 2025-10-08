22311ebcfff944d5a45f8ac40e87b4dbbaddc219
Dekel5030 <dekel5030@gmail.com>
2025-10-08 15:27:07 +0300

    (refactor userservice) added feature of getting users by query params

--- src/UserService/Src/Web.Api/Endpoints/Users/Queries/GetUsers.cs
+++ src/UserService/Src/Web.Api/Endpoints/Users/Queries/GetUsers.cs
@@ -1,6 +1,8 @@
 ﻿
 using Application.Abstractions.Messaging;
+using Application.Users.Queries.Dtos;
 using Application.Users.Queries.GetUserByid;
+using Application.Users.Queries.GetUsers;
 using Kernel;
 using Web.Api.Extensions;
 using Web.Api.Infrastructure;
@@ -11,21 +13,14 @@ public class GetUserById : IEndpoint
 {
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapGet("/api/users/{id:guid}", async (
-            Guid id,
-            IQueryHandler<GetUserByIdQuery, UserReadDto> handler,
+        app.MapGet("/api/users/", async (
+            IQueryHandler<GetUsersQuery, CollectionDto<UserReadDto>> handler,
+            [AsParameters] GetUsersQuery query,
             CancellationToken cancellationToken = default) =>
         {
-            var query = new GetUserByIdQuery(id);
-            Result<UserReadDto> result = await handler.Handle(query, cancellationToken);
+            Result<CollectionDto<UserReadDto>> result = await handler.Handle(query, cancellationToken);
 
             return result.Match(Results.Ok, CustomResults.Problem);
-        });
-
-        app.MapGet("/api/users/", (
-            CancellationToken cancellationToken = default) =>
-        {
-            return Results.Ok("User endpoint is working");
         });
     }
 }
