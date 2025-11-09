40316ae1011b94f6167806514464df29bb35d001
Dekel Rafian <77198015+dekel5030@users.noreply.github.com>
2025-11-09 11:21:41 +0200

    (feature courseservice) added a feature to get courses by query (#101)

---
+++ src/CourseService/Application/Courses/Queries/GetCourses/GetCoursesQueryHandler.cs
@@ -0,0 +1,54 @@
+﻿using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Application.Courses.Queries.Dtos;
+using Kernel;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Courses.Queries.GetCourses;
+
+public class GetCoursesQueryHandler : IQueryHandler<GetCoursesQuery, PagedResponseDto<CourseReadDto>>
+{
+    private readonly IReadDbContext _dbContext;
+
+    public GetCoursesQueryHandler(IReadDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Result<PagedResponseDto<CourseReadDto>>> Handle(
+        GetCoursesQuery request, 
+        CancellationToken cancellationToken = default)
+    {
+        var pageNumber = Math.Max(1, request.PagedQuery.PageNumber ?? 1);
+        var pageSize = Math.Clamp(request.PagedQuery.PageSize ?? 10, 1, 100);
+
+        var baseQuery = _dbContext.Courses;
+
+        var totalItems = await baseQuery.CountAsync(cancellationToken);
+
+        var items = await baseQuery
+            .OrderByDescending(c => c.UpdatedAtUtc)
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .Select(c => new CourseReadDto(
+                c.Id,
+                c.Title,
+                c.Description,
+                c.IsPublished,
+                c.ImageUrl,
+                c.InstructorUserId,
+                c.Price,
+                c.UpdatedAtUtc))
+            .ToListAsync(cancellationToken);
+
+        var dto = new PagedResponseDto<CourseReadDto>
+        {
+            Items = items,
+            PageNumber = pageNumber,
+            PageSize = pageSize,
+            TotalItems = totalItems
+        };
+
+        return Result.Success(dto);
+    }
+}
