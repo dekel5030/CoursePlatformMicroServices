910e957d7ca5b4e6590f0b11715ae9c8e4b2b365
Dekel Rafian <77198015+dekel5030@users.noreply.github.com>
2025-11-17 12:16:01 +0200

    Implement error handling for ValidationError

--- src/UserService/Web.Api/Infrastructure/CustomResults.cs
+++ src/UserService/Web.Api/Infrastructure/CustomResults.cs
@@ -59,8 +59,15 @@ public static class CustomResults
 
         static Dictionary<string, object?>? GetErrors(Result result)
         {
-            // ValidationError is not implemented yet, so we return null for now
-            return null;
+            if (result.Error is not ValidationError validationError)
+            {
+                return null;
+            }
+
+            return new Dictionary<string, object?>
+            {
+                { "errors", validationError.Errors }
+            };
         }
     }
 }
