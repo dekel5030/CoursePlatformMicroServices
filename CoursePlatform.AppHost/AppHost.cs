b902df3bd8de2794bfd69bb8813ac2b9d2d6a4df
Dekel Rafian <77198015+dekel5030@users.noreply.github.com>
2025-11-25 19:06:54 +0200

    (feature apphost) Added apphost using .net aspire to manage the micro… (#155)
    * (feature apphost) Added apphost using .net aspire to manage the microservices and databases in development stages
    
    * (feature apphost) Added a mission to complete in Apphosts using comment
    
    * (Feature apphost) now the apphost is starting the frontend as well
    
    * (Feature apphost) now the apphost is starting the frontend as well
    
    * (refactor frontend) changed hardcoded urls into injectable by aspire

---
+++ CoursePlatform.AppHost/AppHost.cs
@@ -0,0 +1,59 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+// RabbitMq configuration
+var rabbitMq = builder
+    .AddRabbitMQ("rabbitmq")
+    .WithManagementPlugin(15672)
+    .WithDataVolume();
+
+// AuthService configuration
+
+var authDb = builder
+    .AddPostgres("authservice-db")
+    .WithBindMount(@"C:\AspireVolumes\AuthService", "/var/lib/postgresql/data")
+    .AddDatabase("authdb");
+
+var authService = builder
+    .AddProject<Projects.Auth_Api>("authservice")
+    .WithReference(authDb)
+    .WithReference(rabbitMq)
+    .WithEnvironment("ConnectionStrings:Database", authDb.Resource.ConnectionStringExpression)
+    .WithEnvironment("ConnectionStrings:RabbitMq", rabbitMq.Resource.ConnectionStringExpression);
+
+// UserService configuration
+
+var usersDb = builder
+    .AddPostgres("userservice-db")
+    .WithBindMount(@"C:\AspireVolumes\UsersService", "/var/lib/postgresql/data")
+    .AddDatabase("usersdb");
+
+var usersService = builder.AddProject<Projects.User_Api>("userservice")
+    .WithReference(usersDb)
+    .WithReference(rabbitMq)
+    .WithEnvironment("ConnectionStrings:ReadDatabase", usersDb.Resource.ConnectionStringExpression)
+    .WithEnvironment("ConnectionStrings:WriteDatabase", usersDb.Resource.ConnectionStringExpression)
+    .WithEnvironment("ConnectionStrings:RabbitMq", rabbitMq.Resource.ConnectionStringExpression);
+
+
+// CourseService configuration
+
+var coursesDb = builder
+    .AddPostgres("courseservice-db")
+    .WithBindMount(@"C:\AspireVolumes\CoursesService", "/var/lib/postgresql/data")
+    .AddDatabase("coursesdb");
+
+var coursesService = builder.AddProject<Projects.Course_Api>("courseservice")
+    .WithReference(coursesDb)
+    .WithReference(rabbitMq)
+    .WithEnvironment("ConnectionStrings:ReadDatabase", coursesDb.Resource.ConnectionStringExpression)
+    .WithEnvironment("ConnectionStrings:WriteDatabase", coursesDb.Resource.ConnectionStringExpression)
+    .WithEnvironment("ConnectionStrings:RabbitMq", rabbitMq.Resource.ConnectionStringExpression);
+
+var frontend = builder.AddNpmApp("frontend", "../Frontend", "dev")
+    .WithReference(authService)
+    .WithReference(usersService)
+    .WithReference(coursesService)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints();
+
+builder.Build().Run();
