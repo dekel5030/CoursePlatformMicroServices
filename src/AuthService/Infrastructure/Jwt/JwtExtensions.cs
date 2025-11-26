7fc70c14123c9df80ba3a686141efefc995d0321
Dekel Rafian <77198015+dekel5030@users.noreply.github.com>
2025-11-26 22:08:00 +0200

    Feature/Authservice/moved ConfigureJwt into it own file  (#162)
    * (feature orderservice) uncommented section
    
    * (test orderservice) added order aggregate unit tests
    
    * (refactor orderservice) reorganized folder structure
    
    * (refactor authservice) Moved jwt extensions into it own file

---
+++ src/AuthService/Infrastructure/Jwt/JwtExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Jwt;
+
+public static class JwtExtensions
+{
+    public static IServiceCollection ConfigureJwtAuthentication(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.Configure<JwtOptions>(configuration.GetSection(JwtOptions.SectionName));
+        services.ConfigureOptions<JwtBearerOptionsSetup>();
+
+        return services;
+    }
+}
