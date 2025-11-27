ccf2315739a7e6720ee6a336dbd8d465c2413895
Dekel5030 <dekel5030@gmail.com>
2025-11-27 11:06:36 +0200

    (feature userservice) added authenticate and authorization

---
+++ src/UserService/Web.Api/Extensions/AuthenticationExtensions.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace Auth.Api.Extensions;
+
+public static class AuthenticationExtensions
+{
+    public static IServiceCollection AddJwtCookieAuthentication(this IServiceCollection services)
+    {
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer();
+
+        services.AddAuthorization();
+
+        return services;
+    }
+}
