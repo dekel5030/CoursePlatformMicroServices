12d88732e9fa454da9d5ad78d11c28389f37a4eb
Dekel5030 <dekel5030@gmail.com>
2025-11-28 18:57:47 +0200

    (refactor authservice) added di for .net identity

--- src/AuthService/Web.Api/Extensions/AuthenticationExtensions.cs
+++ src/AuthService/Web.Api/Extensions/AuthenticationExtensions.cs
@@ -1,4 +1,8 @@
+using Domain.AuthUsers;
+using Domain.Roles;
+using Infrastructure.Database;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 
 namespace Auth.Api.Extensions;
 
@@ -11,7 +15,11 @@ public static class AuthenticationExtensions
             options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
             options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
         })
+        .AddCookie(IdentityConstants.ApplicationScheme)
         .AddJwtBearer();
+
+        services.AddIdentity<AuthUser, Role>()
+            .AddEntityFrameworkStores<AuthDbContext>();
 
         services.AddAuthorization();
 
