bcf8418194a3d617ff7dd8e9e5ebac093fab119b
Dekel5030 <dekel5030@gmail.com>
2025-11-30 08:38:59 +0200

    (refactor authservice) remove refresh token feature

--- src/AuthService/Application/AuthUsers/Commands/RefreshAccessToken/RefreshAccessTokenCommand.cs
+++
@@ -1,5 +0,0 @@
-using Application.Abstractions.Messaging;
-
-namespace Application.AuthUsers.Commands.RefreshAccessToken;
-
-public record RefreshAccessTokenCommand(string RefreshToken) : ICommand<string>;
