8952b06ade9b4ec64e618951dd0a8c88bfa687a5
Dekel5030 <dekel5030@gmail.com>
2025-11-30 10:13:03 +0200

    feat(authservice): Added an extension methods to convert IdentityResult into our result pattern

---
+++ src/AuthService/Application/Extensions/IdentityResultExtensions.cs
@@ -0,0 +1,56 @@
+﻿using Kernel;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.Extensions;
+
+public static class IdentityResultExtensions
+{
+    public static Result ToApplicationResult(this IdentityResult identityResult)
+    {
+        if (identityResult.Succeeded)
+        {
+            return Result.Success();
+        }
+
+        return Result.Failure(MapErrors(identityResult.Errors));
+    }
+
+    public static Result<T> ToApplicationResult<T>(this IdentityResult identityResult)
+    {
+        if (identityResult.Succeeded)
+        {
+            throw new InvalidOperationException("Cannot convert successful IdentityResult to Result<T> without a value.");
+        }
+
+        return Result<T>.Failure(MapErrors(identityResult.Errors));
+    }
+
+    private static Error MapErrors(IEnumerable<IdentityError> identityErrors)
+    {
+        var errors = identityErrors.Select(e =>
+            new Error(e.Code, e.Description, ResolveErrorType(e.Code)));
+
+        if (errors.Count() == 1) return errors.First();
+        return new ValidationError(errors);
+    }
+
+    private static ErrorType ResolveErrorType(string identityErrorCode)
+    {
+        return identityErrorCode switch
+        {
+            nameof(IdentityErrorDescriber.DuplicateUserName) => ErrorType.Conflict,
+            nameof(IdentityErrorDescriber.DuplicateEmail) => ErrorType.Conflict,
+
+            nameof(IdentityErrorDescriber.DuplicateRoleName) => ErrorType.Conflict,
+            nameof(IdentityErrorDescriber.UserAlreadyInRole) => ErrorType.Conflict,
+            nameof(IdentityErrorDescriber.UserAlreadyHasPassword) => ErrorType.Conflict,
+            nameof(IdentityErrorDescriber.LoginAlreadyAssociated) => ErrorType.Conflict,
+
+            nameof(IdentityErrorDescriber.DefaultError) => ErrorType.Failure,
+            nameof(IdentityErrorDescriber.ConcurrencyFailure) => ErrorType.Failure,
+            nameof(IdentityErrorDescriber.UserLockoutNotEnabled) => ErrorType.Failure,
+
+            _ => ErrorType.Validation
+        };
+    }
+}
