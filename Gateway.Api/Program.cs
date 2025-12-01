d3b32e0e631b8ffb8ecb1b5369d0e66a3f8de3c1
Dekel5030 <dekel5030@gmail.com>
2025-12-01 10:14:48 +0200

    feat(gatewayapi): updated the project configuration in order to work with aspire

--- Gateway.Api/Program.cs
+++ Gateway.Api/Program.cs
@@ -1,9 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
-
+builder.Services.AddServiceDiscovery();
 builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+    .AddServiceDiscoveryDestinationResolver();
 
 var app = builder.Build();
 
