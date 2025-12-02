1c96d24bf0047731ef35cd3b022c7075d692fac7
Dekel5030 <dekel5030@gmail.com>
2025-12-02 19:21:26 +0200

    feat(kernel): moved headernames into a static class

---
+++ CoursePlatform.ServiceDefaults/Auth/HeaderNames.cs
@@ -0,0 +1,8 @@
+﻿namespace CoursePlatform.ServiceDefaults.Auth;
+
+public static class HeaderNames
+{
+    public const string UserIdHeader = "X-User-Id";
+    public const string UserRoleHeader = "X-User-Role";
+    public const string UserPermissionsHeader = "X-User-Permissions";
+}
