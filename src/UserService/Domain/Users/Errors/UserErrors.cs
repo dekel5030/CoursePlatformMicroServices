41167f168fab957f9a4f9a8d880029256c2a61e4
Dekel5030 <dekel5030@gmail.com>
2025-12-03 13:18:16 +0200

    feat(kernel): added forbidden error

--- src/UserService/Domain/Users/Errors/UserErrors.cs
+++ src/UserService/Domain/Users/Errors/UserErrors.cs
@@ -6,4 +6,6 @@ public static class UserErrors
 {
     public static readonly Error NotFound = Error.NotFound("UserService.User.NotFound", "The user was not found.");
     public static readonly Error EmailAlreadyExists = Error.Conflict("UserService.User.EmailAlreadyExists", "The email is already in use by another user.");
+    public static readonly Error Unauthorized = Error.Unauthorized("UserService.User.Unauthorized", "You are not authorized to perform this action.");
+    public static readonly Error Forbidden = Error.Forbidden("UserService.User.Forbidden", "You do not have permission to access this resource.");
 }
