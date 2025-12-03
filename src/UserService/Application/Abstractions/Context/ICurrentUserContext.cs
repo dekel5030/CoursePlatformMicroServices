21c6c98a3482c6edc4f9ecbfe1ef3fc2d40a9a27
Dekel5030 <dekel5030@gmail.com>
2025-12-03 10:53:25 +0200

    feat(userservice): added iusercontext abstraction

---
+++ src/UserService/Application/Abstractions/Context/ICurrentUserContext.cs
@@ -0,0 +1,11 @@
+﻿using Kernel.AuthTypes;
+
+namespace Application.Abstractions.Context;
+
+public interface ICurrentUserContext
+{
+    bool IsAuthenticated { get; }
+    Guid UserId { get; }
+    bool HasUserResourcePermission(ActionType action, Guid resourceId);
+    bool HasRole(RoleType role);
+}
