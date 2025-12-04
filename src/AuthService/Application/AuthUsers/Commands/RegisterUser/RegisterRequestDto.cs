58795d5fa4152805254749f8b139fb9eff238567
Dekel5030 <dekel5030@gmail.com>
2025-12-04 12:12:54 +0200

    feat(authservice): now username is required for registeration and fullname is optional

--- src/AuthService/Application/AuthUsers/Commands/RegisterUser/RegisterRequestDto.cs
+++ src/AuthService/Application/AuthUsers/Commands/RegisterUser/RegisterRequestDto.cs
@@ -4,7 +4,7 @@ public class RegisterRequestDto
 {
     public required string Email { get; set; }
     public required string Password { get; set; }
-    public required string Fullname { get; set; }
-    public string? UserName { get; set; }
+    public required string UserName { get; set; }
+    public string? Fullname { get; set; }
     public string? PhoneNumber { get; set; }
 }
