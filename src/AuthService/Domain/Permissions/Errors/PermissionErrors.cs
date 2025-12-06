4eb9c2e7099e7772d1651a8abe757ffbf83562a1
Dekel5030 <dekel5030@gmail.com>
2025-12-06 12:43:35 +0200

    refactor(authservice): updated Permission errors to be readonly

--- src/AuthService/Domain/Permissions/Errors/PermissionErrors.cs
+++ src/AuthService/Domain/Permissions/Errors/PermissionErrors.cs
@@ -4,7 +4,7 @@ namespace Domain.Permissions.Errors;
 
 public class PermissionErrors
 {
-    internal static readonly Error PermissionNotAssigned = Error.Conflict("Permission.NotAssigned", "The permission is not assigned to the role.");
+    public static readonly Error PermissionNotAssigned = Error.Conflict("Permission.NotAssigned", "The permission is not assigned to the role.");
 
-    public static Error PermissionAlreadyAssigned => Error.Conflict("Permission.AlreadyAssigned", "The permission is already assigned to the role.");
+    public static readonly Error PermissionAlreadyAssigned = Error.Conflict("Permission.AlreadyAssigned", "The permission is already assigned to the role.");
 }
