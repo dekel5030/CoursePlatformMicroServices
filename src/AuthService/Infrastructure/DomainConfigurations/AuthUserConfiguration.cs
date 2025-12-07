a2c5693ba7ae52bbd1cdc8f412857e0adece7bd6
Dekel5030 <dekel5030@gmail.com>
2025-12-07 20:21:08 +0200

    feat(authservice): updated authuser ef config

--- src/AuthService/Infrastructure/DomainConfigurations/AuthUserConfiguration.cs
+++ src/AuthService/Infrastructure/DomainConfigurations/AuthUserConfiguration.cs
@@ -1,4 +1,6 @@
 ﻿using Domain.AuthUsers;
+using Domain.Permissions;
+using Kernel.Auth.AuthTypes;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -9,10 +11,43 @@ internal sealed class AuthUserConfiguration : IEntityTypeConfiguration<AuthUser>
     public void Configure(EntityTypeBuilder<AuthUser> user)
     {
         user.ToTable("auth_users");
-
-        user.Ignore(u => u.DomainEvents);
-        user.Ignore(u => u.Roles);
-
         user.Property(u => u.Id).ValueGeneratedNever();
+        user.Ignore(u => u.DomainEvents);
+        //user.Ignore(u => u.Roles);
+
+        user.OwnsMany(u => u.Permissions, permissionBuilder =>
+        {
+            permissionBuilder.ToTable("user_permissions");
+
+            permissionBuilder.WithOwner().HasForeignKey("UserId");
+            permissionBuilder.HasKey(
+                "UserId",
+                nameof(UserPermission.Resource),
+                nameof(UserPermission.Action),
+                nameof(UserPermission.ResourceId));
+
+            permissionBuilder.Property(p => p.Effect)
+                .HasConversion<string>();
+
+            permissionBuilder.Property(p => p.Resource)
+                .HasConversion<string>()
+                .HasMaxLength(50)
+                .IsRequired();
+
+            permissionBuilder.Property(p => p.Action)
+                .HasConversion<string>()
+                .HasMaxLength(50)
+                .IsRequired();
+
+            permissionBuilder.Property(p => p.ResourceId)
+                .HasColumnName("resource_id")
+                .HasMaxLength(100)
+                .IsRequired()
+                .HasConversion(
+                    resourceId => resourceId.Value,
+                    value => ResourceId.Create(value)
+                );
+        });
+
     }
 }
