acb3b304cc4b18e4991b4c317861e4b80397ef71
Dekel5030 <dekel5030@gmail.com>
2025-12-09 12:18:03 +0200

    feat(authservice): added logout use case

--- src/AuthService/Application/AuthUsers/Queries/GetCurrentUser/GetCurrentUserQueryHandler.cs
+++ src/AuthService/Application/AuthUsers/Queries/GetCurrentUser/GetCurrentUserQueryHandler.cs
@@ -1,21 +1,24 @@
-using Application.Abstractions.Identity;
+using Application.Abstractions.Data;
 using Application.Abstractions.Messaging;
 using Application.AuthUsers.Dtos;
-using Domain.AuthUsers;
 using Domain.AuthUsers.Errors;
 using Kernel;
 using Kernel.Auth.Abstractions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.AuthUsers.Queries.GetCurrentUser;
 
 public class GetCurrentUserQueryHandler : IQueryHandler<GetCurrentUserQuery, CurrentUserDto>
 {
     private readonly IUserContext _userContext;
+    private readonly IReadDbContext _dbContext;
 
     public GetCurrentUserQueryHandler(
-        IUserContext userContext)
+        IUserContext userContext,
+        IReadDbContext dbContext)
     {
         _userContext = userContext;
+        _dbContext = dbContext;
     }
 
     public async Task<Result<CurrentUserDto>> Handle(
@@ -24,6 +27,20 @@ public class GetCurrentUserQueryHandler : IQueryHandler<GetCurrentUserQuery, Cur
     {
         Guid? userId = _userContext.UserId;
 
-        return Result.Success(new CurrentUserDto(Guid.Empty, ""));
+        if (userId == null)
+        {
+            return Result.Failure<CurrentUserDto>(AuthUserErrors.Unauthorized);
+        }
+
+        var user = await _dbContext.AuthUsers.FirstOrDefaultAsync(
+            u => u.Id == userId.Value,
+            cancellationToken);
+
+        var response = new CurrentUserDto(
+            Id: user!.Id,
+            Email: user.Email,
+            UserName: user.UserName);
+
+        return Result.Success(response);
     }
 }
