c680fb3ea347c5ba8e5e390fe5870836d620f826
Dekel5030 <dekel5030@gmail.com>
2025-12-17 15:29:53 +0200

    feat(authservice): now permission is an value object

--- src/AuthService/Domain/Permissions/Permission.cs
+++ src/AuthService/Domain/Permissions/Permission.cs
@@ -5,15 +5,15 @@ using Kernel.Auth.AuthTypes;
 
 namespace Domain.Permissions;
 
-public class Permission : IEquatable<Permission>
+public record Permission
 {
-    public int Id { get; private set; }
     public EffectType Effect { get; private set; }
     public ActionType Action { get; private set; }
     public ResourceType Resource { get; private set; }
     public ResourceId ResourceId { get; private set; }
+    public string Key => $"{(int)Effect:D1}{(int)Action:D2}{(int)Resource:D3}{ResourceId.Value}";
 
-    #pragma warning disable CS8618 
+    #pragma warning disable CS8618
     private Permission() { }
     #pragma warning restore CS8618 
 
@@ -59,30 +59,6 @@ public class Permission : IEquatable<Permission>
         var resourceId = ResourceId.Create(resourceIdSegment);
 
         return Result<Permission>.Success(new Permission(effect, action, resource, resourceId));
-    }
-
-    public bool Equals(Permission? other)
-    {
-        if (other is null) return false;
-        if (ReferenceEquals(this, other)) return true;
-
-        return Action == other.Action &&
-               Resource == other.Resource &&
-               ResourceId == other.ResourceId &&
-               Effect == other.Effect;
-    }
-
-    public override bool Equals(object? obj)
-    {
-        if (obj is null) return false;
-        if (ReferenceEquals(this, obj)) return true;
-        if (obj.GetType() != this.GetType()) return false;
-        return Equals((Permission)obj);
-    }
-
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(Action, Resource, ResourceId, Effect);
     }
 
     public override string ToString()
