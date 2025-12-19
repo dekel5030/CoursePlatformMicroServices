bb6c295dd634ca542c381a38807744106f05d220
Dekel5030 <dekel5030@gmail.com>
2025-12-19 09:18:05 +0200

    feat(authservice): added an permission resolver that flattens the permissions for the jwt

---
+++ src/AuthService/Application/Abstractions/Auth/IPermissionResolver.cs
@@ -0,0 +1,8 @@
+﻿using Domain.AuthUsers;
+
+namespace Application.Abstractions.Auth;
+
+public interface IPermissionResolver
+{
+    IEnumerable<string> ResolveEffectivePermissions(AuthUser user);
+}
