ff9bce05b3ba28886d1f478d34f7180ddfb655a2
Dekel5030 <dekel5030@gmail.com>
2025-12-19 07:22:32 +0200

    feat(authservice): added necessary abstractions in application layer for the jwt token creation

---
+++ src/AuthService/Application/Abstractions/Auth/ITokenProvider.cs
@@ -0,0 +1,11 @@
+﻿using Domain.AuthUsers;
+
+namespace Application.Abstractions.Auth;
+
+public interface ITokenProvider
+{
+    string GenerateInternalToken(
+        AuthUser user,
+        IEnumerable<string> permissions, 
+        DateTime expiration);
+}
