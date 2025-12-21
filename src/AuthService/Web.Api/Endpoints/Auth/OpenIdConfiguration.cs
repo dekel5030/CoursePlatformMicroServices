f159abe36c06ff9471efb5dae6e66f98c6c5de17
Dekel5030 <dekel5030@gmail.com>
2025-12-22 00:26:59 +0200

    feat(authservice): add service discovery endpoint

---
+++ src/AuthService/Web.Api/Endpoints/Auth/OpenIdConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace Auth.Api.Endpoints.Auth;
+
+public class OpenIdConfigurationEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/.well-known/openid-configuration", (HttpContext context) =>
+        {
+            var baseUrl = $"{context.Request.Scheme}://{context.Request.Host}";
+
+            return Results.Ok(new
+            {
+                issuer = "course-platform-auth",
+                jwks_uri = $"{baseUrl}/.well-known/jwks.json",
+                response_types_supported = new[] { "id_token" },
+                id_token_signing_alg_values_supported = new[] { "RS256" }
+            });
+        })
+        .AllowAnonymous();
+    }
+}
