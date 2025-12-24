ae81281170d2f2092db5dc369ec10d3267a2f794
Dekel5030 <dekel5030@gmail.com>
2025-12-24 21:29:12 +0200

    feat(authservice): add get role by name endpoint

---
+++ src/AuthService/Web.Api/Endpoints/Roles/GetRoleByName.cs
@@ -0,0 +1,24 @@
+﻿using Auth.Api.Extensions;
+using Auth.Api.Infrastructure;
+using Auth.Application.Roles.Queries.GetRoleByName;
+using Kernel;
+using Kernel.Messaging.Abstractions;
+
+namespace Auth.Api.Endpoints.Roles;
+
+public class GetRoleByName : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/roles/{roleName}", async (
+            string roleName,
+            IMediator mediator,
+            CancellationToken cancellationToken) =>
+        {
+            var query = new GetRoleByNameQuery(roleName);
+            Result<RoleDto> result = await mediator.Send(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        });
+    }
+}
