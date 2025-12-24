fd2615a1b85e726dd35d54c204bcd35f2f0f4f63
Dekel Rafian <77198015+dekel5030@users.noreply.github.com>
2025-12-24 21:12:52 +0200

    feat(authservice): get user by id query (#293)
    * feat(authservice): added get user dtos and query handler
    
    * feat(authservice): added get user by id endpoint

---
+++ src/AuthService/Application/AuthUsers/Queries/GetUserByIdQuery.cs
@@ -0,0 +1,5 @@
+﻿using Kernel.Messaging.Abstractions;
+
+namespace Auth.Application.AuthUsers.Queries;
+
+public record GetUserByIdQuery(Guid UserId) : IQuery<UserDto>;
---
+++ src/AuthService/Web.Api/Endpoints/Users/GetUserById.cs
@@ -0,0 +1,24 @@
+﻿using Auth.Api.Extensions;
+using Auth.Api.Infrastructure;
+using Auth.Application.AuthUsers.Queries;
+using Kernel;
+using Kernel.Messaging.Abstractions;
+
+namespace Auth.Api.Endpoints.Users;
+
+public class GetUserById : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/users/{id:guid}", async (
+            Guid id,
+            IMediator mediator,
+            CancellationToken cancellationToken) =>
+        {
+            var query = new GetUserByIdQuery(id);
+            Result<UserDto> result = await mediator.Send(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        });
+    }
+}
