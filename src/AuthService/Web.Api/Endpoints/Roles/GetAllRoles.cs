b3ca33ccc474197604d4323e6d81b5ce964318ee
Dekel5030 <dekel5030@gmail.com>
2025-12-24 20:43:59 +0200

    feat(authservice): added getallroles endpoint

---
+++ src/AuthService/Web.Api/Endpoints/Roles/GetAllRoles.cs
@@ -0,0 +1,25 @@
+﻿using Auth.Api.Extensions;
+using Auth.Api.Infrastructure;
+using Auth.Application.Roles.Queries.GetAllRoles;
+using Kernel.Messaging.Abstractions;
+
+namespace Auth.Api.Endpoints.Roles;
+
+public class GetAllRoles : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/roles", async (
+            IMediator mediator) =>
+        {
+            var query = new GetAllRolesQuery();
+
+            var result = await mediator.Send(query);
+
+            return result.Match(
+                roles => Results.Ok(roles),
+                error => CustomResults.Problem(error)
+            );
+        });
+    }
+}
