d03227768932ee16cb5035efea09f9bed3563f72
Dekel5030 <dekel5030@gmail.com>
2025-12-30 13:37:34 +0200

    feat(courseservice): add featured courses repo

---
+++ src/CourseService/Application/Abstractions/Data/Repositories/IFeaturedCoursesRepository.cs
@@ -0,0 +1,8 @@
+﻿using Courses.Domain.Courses;
+
+namespace Courses.Application.Abstractions.Data.Repositories;
+
+public interface IFeaturedCoursesRepository
+{
+    Task<IReadOnlyList<Course>> GetFeaturedCourse();
+}
