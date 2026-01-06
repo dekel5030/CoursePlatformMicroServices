d2a8ee237d2c469a2e5c14ac33b80c7d54887225
Dekel5030 <dekel5030@gmail.com>
2026-01-06 15:48:42 +0200

    feat(storageservice): added swagger

--- src/StorageService/Program.cs
+++ src/StorageService/Program.cs
@@ -1,14 +1,22 @@
 using CoursePlatform.ServiceDefaults;
 using CoursePlatform.ServiceDefaults.Endpoints;
+using CoursePlatform.ServiceDefaults.Swagger;
 using StorageService.S3;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
+builder.AddDefaultOpenApi();
 builder.Services.ConfigureS3(builder.Configuration);
 builder.Services.AddEndpoints(typeof(Program).Assembly);
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapDefaultEndpoints();
 app.MapEndpoints();
