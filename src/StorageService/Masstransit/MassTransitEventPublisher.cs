37d62d5e7b6086dfdd4948829e4792ebd17c8efe
Dekel5030 <dekel5030@gmail.com>
2026-01-06 17:05:23 +0200

    feat(storageservice): registered masstransit

---
+++ src/StorageService/Masstransit/MassTransitEventPublisher.cs
@@ -0,0 +1,25 @@
+﻿using Kernel.EventBus;
+using MassTransit;
+
+namespace Auth.Infrastructure.MassTransit;
+
+internal sealed class MassTransitEventPublisher : IEventBus
+{
+    private readonly IPublishEndpoint _publishEndpoint;
+    private readonly ILogger<MassTransitEventPublisher> _logger;
+
+    public MassTransitEventPublisher(
+        IPublishEndpoint publishEndpoint,
+        ILogger<MassTransitEventPublisher> logger)
+    {
+        _publishEndpoint = publishEndpoint;
+        _logger = logger;
+    }
+
+    public async Task PublishAsync<TEvent>(TEvent @event, CancellationToken cancellationToken = default)
+        where TEvent : class
+    {
+        _logger.LogInformation("Publishing event of type {EventType}: {Event}", @event.GetType(), @event);
+        await _publishEndpoint.Publish(@event, cancellationToken);
+    }
+}
