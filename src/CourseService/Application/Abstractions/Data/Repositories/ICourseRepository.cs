e9dc98ca2f04704814f62e5176b61d2100cb1efa
Dekel5030 <dekel5030@gmail.com>
2026-01-08 23:10:36 +0200

    faet(courseservice): add repositories abstractions

---
+++ src/CourseService/Application/Abstractions/Data/Repositories/ICourseRepository.cs
@@ -0,0 +1,10 @@
+﻿using Courses.Domain.Courses;
+using Courses.Domain.Courses.Primitives;
+
+namespace Courses.Application.Abstractions.Data.Repositories;
+
+public interface ICourseRepository : IRepository<Course, CourseId>
+{
+    void Add(Course entity);
+}
+
---
+++ src/CourseService/Application/Abstractions/Data/Repositories/ILessonRepository.cs
@@ -0,0 +1,10 @@
+﻿using Courses.Domain.Lessons;
+using Courses.Domain.Lessons.Primitives;
+
+namespace Courses.Application.Abstractions.Data.Repositories;
+
+public interface ILessonRepository : IRepository<Lesson, LessonId>
+{
+
+}
+
