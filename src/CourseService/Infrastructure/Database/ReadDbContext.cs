4fae1f6aaaa69486ac8d25c0fe285117c4e3da31
Dekel5030 <dekel5030@gmail.com>
2026-01-08 17:19:49 +0200

    refactor(courseservice): added dbcontext base to reduce code duplication

--- src/CourseService/Infrastructure/Database/ReadDbContext.cs
+++ src/CourseService/Infrastructure/Database/ReadDbContext.cs
@@ -1,29 +1,16 @@
 ﻿using Courses.Application.Abstractions.Data;
-using Courses.Domain.Courses;
-using Courses.Domain.Lessons;
 using Microsoft.EntityFrameworkCore;
 
 namespace Courses.Infrastructure.Database;
 
-public class ReadDbContext : DbContext, IReadDbContext
+public class ReadDbContext : AppDbContextBase, IReadDbContext
 {
     public ReadDbContext(DbContextOptions<ReadDbContext> options) : base(options)
     {
-
     }
-
-    public DbSet<Course> Courses { get; set; }
-    public DbSet<Lesson> Lessons { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DependencyInjection).Assembly);
     }
 }
--- src/CourseService/Infrastructure/Database/WriteDbContext.cs
+++ src/CourseService/Infrastructure/Database/WriteDbContext.cs
@@ -1,28 +1,20 @@
 ﻿using Courses.Application.Abstractions.Data;
-using Courses.Domain.Courses;
-using Courses.Domain.Lessons;
-using Courses.Infrastructure.DomainEvents;
 using Kernel;
-using MassTransit;
+using Kernel.Messaging.Abstractions;
 using Microsoft.EntityFrameworkCore;
 
 namespace Courses.Infrastructure.Database;
 
-public sealed class WriteDbContext(
-    DbContextOptions<WriteDbContext> options)
-        : DbContext(options), IWriteDbContext
+public sealed class WriteDbContext : AppDbContextBase, IWriteDbContext
 {
-    public DbSet<Course> Courses { get; set; }
-    public DbSet<Lesson> Lessons { get; set; }
+    private readonly IMediator _mediator;
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    public WriteDbContext(
+        DbContextOptions<WriteDbContext> options,
+        IMediator mediator) 
+        : base(options)
     {
-        base.OnModelCreating(modelBuilder);
-
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(WriteDbContext).Assembly);
-
-        modelBuilder.AddTransactionalOutboxEntities();
-        AssignConcurrencyToken(modelBuilder);
+        _mediator = mediator;
     }
 
     public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
@@ -48,19 +40,9 @@ public sealed class WriteDbContext(
             entity.ClearDomainEvents();
         }
 
-        //return dispatcher.DispatchAsync(domainEvents, cancellationToken);
-        return Task.CompletedTask;
-    }
-
-    private void AssignConcurrencyToken(ModelBuilder modelBuilder)
-    {
-        foreach (var et in modelBuilder.Model.GetEntityTypes()
-             .Where(t => typeof(IVersionedEntity).IsAssignableFrom(t.ClrType)))
-        {
-            modelBuilder.Entity(et.ClrType)
-                .Property<long>(nameof(IVersionedEntity.EntityVersion))
-                .IsConcurrencyToken()
-                .HasColumnName("entity_version");
-        }
+        IEnumerable<Task> tasks = domainEvents
+            .Select(domainEvent => _mediator.Publish(domainEvent, cancellationToken));
+    
+        return Task.WhenAll(tasks);
     }
 }
