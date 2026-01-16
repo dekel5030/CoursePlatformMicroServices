64dd104609fac79c8f55b3b5f3bfe60396fcf014
Dekel Rafian <77198015+dekel5030@users.noreply.github.com>
2026-01-16 09:33:34 +0200

    feat(courseservice) get featured courses returns allowed actions (#416)
    * style(courseservice): removed commented old code
    
    * feat(courseservice): get courses handler now using the action provider
    
    * feat(courseservice): updated the api mapping extension to use the allowed actions response
    
    * bugfix(courseservice): course CanBePublished base on lesson count primitve
    
    * feat(coureservice): get featured courses support dynamic allowed actions

--- src/CourseService/Application/Courses/Queries/GetFeatured/GetFeaturedQueryHandler.cs
+++ src/CourseService/Application/Courses/Queries/GetFeatured/GetFeaturedQueryHandler.cs
@@ -1,43 +1,60 @@
-//using Courses.Application.Abstractions.Repositories;
-//using Courses.Application.Abstractions.Storage;
-//using Courses.Application.Courses.Extensions;
-//using Courses.Application.Courses.Queries.Dtos;
-//using Courses.Application.Shared.Dtos;
-//using Courses.Domain.Courses;
-//using Kernel;
-//using Kernel.Messaging.Abstractions;
+using Courses.Application.Abstractions.Repositories;
+using Courses.Application.Abstractions.Storage;
+using Courses.Application.Actions.Abstract;
+using Courses.Application.Courses.Queries.Dtos;
+using Courses.Application.Shared.Dtos;
+using Courses.Domain.Courses;
+using Kernel;
+using Kernel.Messaging.Abstractions;
 
-//namespace Courses.Application.Courses.Queries.GetFeatured;
+namespace Courses.Application.Courses.Queries.GetFeatured;
 
-//public class GetFeaturedQueryHandler : IQueryHandler<GetFeaturedQuery, CourseCollectionDto>
-//{
-//    private readonly IFeaturedCoursesRepository _featuredCoursesProvider;
-//    private readonly IStorageUrlResolver _urlResolver;
+public class GetFeaturedQueryHandler : IQueryHandler<GetFeaturedQuery, CourseCollectionDto>
+{
+    private readonly IFeaturedCoursesRepository _featuredCoursesProvider;
+#pragma warning disable S4487 // Unread "private" fields should be removed
+    private readonly IStorageUrlResolver _urlResolver;
+#pragma warning restore S4487 // Unread "private" fields should be removed
+    private readonly ICourseActionProvider _actionProvider;
 
-//    public GetFeaturedQueryHandler(
-//        IFeaturedCoursesRepository featuredCoursesProvider,
-//        IStorageUrlResolver urlResolver)
-//    {
-//        _featuredCoursesProvider = featuredCoursesProvider;
-//        _urlResolver = urlResolver;
-//    }
+    public GetFeaturedQueryHandler(
+        IFeaturedCoursesRepository featuredCoursesProvider,
+        IStorageUrlResolver urlResolver,
+        ICourseActionProvider actionProvider)
+    {
+        _featuredCoursesProvider = featuredCoursesProvider;
+        _urlResolver = urlResolver;
+        _actionProvider = actionProvider;
+    }
 
-//    public async Task<Result<PagedResponseDto<CourseSummaryDto>>> Handle(
-//        GetFeaturedQuery request,
-//        CancellationToken cancellationToken = default)
-//    {
-//        IReadOnlyList<Course> courses = await _featuredCoursesProvider.GetFeaturedCourse();
+    public async Task<Result<CourseCollectionDto>> Handle(
+        GetFeaturedQuery request,
+        CancellationToken cancellationToken = default)
+    {
+        IReadOnlyList<Course> courses = await _featuredCoursesProvider.GetFeaturedCourse();
 
-//        List<CourseSummaryDto> courseDtos = await courses.ToSummaryDtosAsync(_urlResolver, cancellationToken);
+        var courseDtos = courses
+             .Select(course => new CourseSummaryDto(
+                 course.Id,
+                 course.Title,
+                 course.InstructorId?.ToString(),
+                 course.Price.Amount,
+                 course.Price.Currency,
+                 course.Images.Count > 0 ? course.Images[0] : null,
+                 course.LessonCount,
+                 course.EnrollmentCount,
+                 _actionProvider.GetAllowedActions(course)))
+             .ToList();
 
-//        var response = new PagedResponseDto<CourseSummaryDto>
-//        {
-//            Items = courseDtos,
-//            PageNumber = 1,
-//            PageSize = Math.Max(1, courseDtos.Count),
-//            TotalItems = courseDtos.Count
-//        };
+        var response = new CourseCollectionDto
+        (
+            Items: courseDtos,
+            PageNumber: 1,
+            PageSize: courseDtos.Count,
+            TotalItems: courseDtos.Count,
+            AllowedActions: _actionProvider.GetAllowedCollectionActions()
+        );
 
-//        return Result.Success(response);
-//    }
-//}
+        return Result.Success(response);
+    }
+}
