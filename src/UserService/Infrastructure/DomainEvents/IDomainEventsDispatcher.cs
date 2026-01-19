db20d58e3ff85c4e607724c9e795a5fcec223b64
Dekel5030 <dekel5030@gmail.com>
2026-01-19 08:36:21 +0200

    refactor(userservice): removed unused old code

--- src/UserService/Infrastructure/DomainEvents/IDomainEventsDispatcher.cs
+++
@@ -1,8 +0,0 @@
-﻿using Kernel.Messaging.Abstractions;
-
-namespace Users.Infrastructure.DomainEvents;
-
-public interface IDomainEventsDispatcher
-{
-    Task DispatchAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken cancellationToken = default);
-}
