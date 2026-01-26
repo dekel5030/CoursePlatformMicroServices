d2e51b903eafcfeff86c2b50ae532652aed0ffe1
Dekel5030 <dekel5030@gmail.com>
2026-01-26 10:36:17 +0200

    feat(courseservice): added transcript line object

---
+++ src/CourseService/Domain/Lessons/Primitives/TranscriptLine.cs
@@ -0,0 +1,8 @@
+﻿namespace Courses.Domain.Lessons.Primitives;
+
+public sealed record TranscriptLine
+{
+    public TimeSpan Start { get; init; }
+    public TimeSpan End { get; init; }
+    public string Text { get; init; } = string.Empty;
+}
