901625bd7853f15e220d2420e1f1a76152f8a97a
Dekel5030 <dekel5030@gmail.com>
2026-01-28 11:03:16 +0200

    refactor(courseservice): now enrollment is aggregate

--- src/CourseService/Domain/Enrollments/Errors/EnrollmentErrors.cs
+++ src/CourseService/Domain/Enrollments/Errors/EnrollmentErrors.cs
@@ -7,4 +7,8 @@ public static class EnrollmentErrors
     public static readonly Error UnPublishedCourse = Error.Validation(
         "Enrollment.CourseUnpublished",
         "Cannot enroll in unpublished course");
+
+    public static readonly Error InvalidExpirationDate = Error.Validation(
+        "Enrollment.InvalidExpirationDate",
+        "Expiration date must be after enrollment date");
 }
