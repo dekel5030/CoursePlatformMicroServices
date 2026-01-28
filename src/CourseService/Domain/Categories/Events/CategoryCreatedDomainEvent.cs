0014f67549cc968811c7a07cffeb958118d71969
Dekel5030 <dekel5030@gmail.com>
2026-01-28 13:12:46 +0200

    feat(courseservice): added category domain events

---
+++ src/CourseService/Domain/Categories/Events/CategoryCreatedDomainEvent.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Kernel.Messaging.Abstractions;
+
+namespace Courses.Domain.Categories.Events;
+public sealed record CategoryCreatedDomainEvent(ICategorySnapshot Category) : IDomainEvent;
---
+++ src/CourseService/Domain/Categories/ICategorySnapshot.cs
@@ -0,0 +1,11 @@
+﻿using Courses.Domain.Categories.Primitives;
+using Courses.Domain.Shared.Primitives;
+
+namespace Courses.Domain.Categories;
+
+public interface ICategorySnapshot
+{
+    CategoryId Id { get; }
+    string Name { get; }
+    Slug Slug { get; }
+}
