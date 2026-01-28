62b4df26d754e4bb50f5d2f0ff6636d8ddd61825
Dekel5030 <dekel5030@gmail.com>
2026-01-28 13:09:30 +0200

    feat(courseservice): added module domain events

--- src/CourseService/Domain/Lessons/Events/LessonVideoDataUpdatedDomainEvent.cs
+++
@@ -1,5 +0,0 @@
-﻿using Kernel.Messaging.Abstractions;
-
-namespace Courses.Domain.Lessons.Events;
-
-public record LessonVideoDataUpdatedDomainEvent(Lesson Lesson) : IDomainEvent;
---
+++ src/CourseService/Domain/Module/Events/ModuleCreateDomainEvent.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Kernel.Messaging.Abstractions;
+
+namespace Courses.Domain.Module.Events;
+
+public sealed record ModuleCreateDomainEvent(IModuleSnapshot Module) : IDomainEvent;
