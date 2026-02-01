690da3a29cb995e9a0fa912d4c00e925c3aea457
Dekel5030 <dekel5030@gmail.com>
2026-02-01 19:53:17 +0200

    refactor(courseservice): removed unused code

--- src/CourseService/Infrastructure/DomainEvents/IDomainEventsDispatcher.cs
+++
@@ -1,8 +0,0 @@
-﻿using Kernel.Messaging.Abstractions;
-
-namespace Courses.Infrastructure.DomainEvents;
-
-public interface IDomainEventsDispatcher
-{
-    Task DispatchAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken cancellationToken = default);
-}
